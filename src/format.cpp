1ab0ff12e3eb3a21e494b73c3fdf01e613c9bd94
dsardelic <25157560+dsardelic@users.noreply.github.com>
2023-12-26 22:56:12 +0100

    Implement formatting for elapsed time

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,20 @@
-#include <string>
-
 #include "format.h"
 
-using std::string;
+#include <iomanip>  // std::setfill, std::setw
+#include <sstream>  // std::stringstream
+#include <string>   // std::string
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+std::string Format::ElapsedTime(long seconds) {
+  long hours = seconds / 3600;
+  hours %= 100;  // display limited to 2 digits
+  seconds %= 3600;
+  short minutes = seconds / 60;
+  seconds %= 60;
+
+  std::stringstream ss;
+  ss << std::setfill('0');
+  ss << std::setw(2) << hours << ":";
+  ss << std::setw(2) << minutes << ":";
+  ss << std::setw(2) << seconds;
+  return ss.str();
+}
