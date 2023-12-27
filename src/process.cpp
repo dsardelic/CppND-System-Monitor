251b1221ba4f944ecef9f99fe20b8c65f2afa85c
dsardelic <25157560+dsardelic@users.noreply.github.com>
2023-12-27 01:27:35 +0100

    Coupling: Process <-> LinuxParser

--- src/process.cpp
+++ src/process.cpp
@@ -1,33 +1,24 @@
-#include <unistd.h>
-#include <cctype>
-#include <sstream>
-#include <string>
-#include <vector>
-
 #include "process.h"
 
-using std::string;
-using std::to_string;
-using std::vector;
+#include <string>
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+#include "linux_parser.h"
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+int Process::Pid() const { return pid_; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+float Process::CpuUtilization() const {
+  return static_cast<float>(LinuxParser::ActiveJiffies(pid_)) /
+         LinuxParser::ActiveJiffies();
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+std::string Process::Command() { return LinuxParser::Command(pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+std::string Process::Ram() { return LinuxParser::Ram(pid_); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+std::string Process::User() { LinuxParser::User(pid_); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+long int Process::UpTime() { LinuxParser::UpTime(pid_); }
+
+bool Process::operator<(Process const& rhs) const {
+  return this->CpuUtilization() < rhs.CpuUtilization();
+}
